511e1f92b721e560e7c08ecd2a88202aeb7d1217
Mikhaylo Kisil <mihajlokisil20@gmail.com>
2024-11-08 21:01:47 +0200

    Added max bg image size for qrcode

--- src/apps/qrcode/forms.py
+++ src/apps/qrcode/forms.py
@@ -3,6 +3,7 @@ import re
 
 class QRCodeGenerateForm(forms.Form):
     SAVE_FORMATS = ['.png', '.jpg', '.svg', '.gif']
+    MAX_BACKGROUND_IMG_SIZE = 5 * 1024 * 1024  # 10 MB
 
     content = forms.CharField(
         max_length=2500,
@@ -42,6 +43,12 @@ class QRCodeGenerateForm(forms.Form):
             raise forms.ValidationError('Invalid color format. Use hex format (e.g., #FFFFFF or #FFF).')
 
         return color
+
+    def clean_background_img(self):
+        image = self.cleaned_data.get("background_img")
+        if image.size > self.MAX_BACKGROUND_IMG_SIZE:
+            raise forms.ValidationError(f"Max background image size is {self.MAX_BACKGROUND_IMG_SIZE} bytes.")
+        return image
 
     def clean_save_format(self):
         save_format = self.cleaned_data.get('save_format')
