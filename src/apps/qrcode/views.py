f5c2e391b20c79a354ceaafec4fb07fc174f731e
Mikhaylo Kisil <mihajlokisil20@gmail.com>
2024-11-10 13:24:29 +0200

    Fixed bug

--- src/apps/qrcode/views.py
+++ src/apps/qrcode/views.py
@@ -35,8 +35,10 @@ class QRCodeGenerateView(FormView):
         save_format = form.cleaned_data.get('save_format')
 
         qrcode = segno.make(content, error='m')
-        if int(qrcode.version) <= 10:
+        if isinstance(qrcode.version, int) and qrcode.version <= 10:
             scale = 8
+        elif isinstance(qrcode.version, int) or qrcode.version in ['M1', 'M2', 'M3', 'M4']:
+            scale = 5
         else:
             scale = 5
 
